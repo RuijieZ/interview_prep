7e164f64f25a97b0a19069162a6ab2fa83b28436
RuijieZ <ruijieApplication@gmail.com>
2017-11-19 15:27:49 -0500

    greedy soln does not work, comment out greedy soln

--- byte_by_byte/min_coin_to_composite_change.py
+++ byte_by_byte/min_coin_to_composite_change.py
@@ -6,27 +6,30 @@
 #	   ->  min(f(x-1), f(x-5), f(x-10), f(x-25))
 
 
-# greedy soln
-# works because there is a coin 1 inside
-coins = [25, 10, 5, 1]
+# # greedy soln
+# # works because there is a coin 1 inside
+# coins = [9,7,1]
 
-def min_coins_for_change(x, coin_count):
-	for coin in coins:
-		if x >= coin:
-			if x % coin == 0:
-				return int(x / coin) + coin_count
-			else:
-				return min_coins_for_change(x % coin, int(x / coin) + coin_count)
+# def min_coins_for_change(x, coin_count):
+# 	for coin in coins:
+# 		if x >= coin:
+# 			if x % coin == 0:
+# 				return int(x / coin) + coin_count
+# 			else:
+# 				return min_coins_for_change(x % coin, int(x / coin) + coin_count)
 
 
-# test
-print(min_coins_for_change(100, 0)) # 4
-print(min_coins_for_change(99, 0)) # 75 + 20 + 1 * 4  should be 9
-print(min_coins_for_change(85, 0)) # 75 + 10   should be 4
+# # test
+# print(min_coins_for_change(15, 0)) # 3
+# print(min_coins_for_change(99, 0)) # 75 + 20 + 1 * 4  should be 9
+# print(min_coins_for_change(85, 0)) # 75 + 10   should be 4
+
+# note the above greedy slon will fail on the case of 15 with coin system [9,7,1], so only correct soln is the below dynanmic
+# soln
 
 
 # dynanmic programming soln
-coins = [25, 10, 5, 1]
+coins = [9,7,1]
 
 def min_coins_for_change_v2(x):
 	table = [-1 for i in range(x+1)]
@@ -40,6 +43,6 @@ def min_coins_for_change_v2(x):
 			return table[x]
 	return min_coin_rec(x)
 
-print(min_coins_for_change_v2(100)) # 4
-print(min_coins_for_change_v2(99)) # 75 + 20 + 1 * 4  should be 9
-print(min_coins_for_change_v2(85)) # 75 + 10   should be 4
+print(min_coins_for_change_v2(15)) # 4
+print(min_coins_for_change_v2(99)) # 11 * 9
+print(min_coins_for_change_v2(85)) # 63 + 21 + 1   should be 11
